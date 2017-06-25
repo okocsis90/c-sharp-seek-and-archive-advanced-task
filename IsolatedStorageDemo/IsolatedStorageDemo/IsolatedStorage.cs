57752e10f9c0b2e64865ff27f16f07575f125544
okocsis90 <o.kocsis90@gmail.com>
2017-06-25 15:34:02 +0200

    isolatedStorage demo completed

---
+++ IsolatedStorageDemo/IsolatedStorageDemo/IsolatedStorage.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.IsolatedStorage;
+
+namespace IsolatedStorageDemo
+{
+    class IsolatedStorage
+    {
+        static void Main(string[] args)
+        {
+            IsolatedStorageFile userStore = IsolatedStorageFile.GetUserStoreForAssembly();
+
+            IsolatedStorageFileStream userStream = new IsolatedStorageFileStream("UserSettings.set", FileMode.Create, userStore);
+
+            StreamWriter userWriter = new StreamWriter(userStream);
+            userWriter.WriteLine("User Prefs");
+            userWriter.Close();
+
+            string[] files = userStore.GetFileNames("UserSettings.set");
+            if (files.Length == 0)
+            {
+                Console.WriteLine("Oops, no such file found.");
+            }
+            else
+            {
+                userStream = new IsolatedStorageFileStream("UserSettings.set", FileMode.Open, userStore);
+
+                StreamReader userReader = new StreamReader(userStream);
+                string contents = userReader.ReadToEnd();
+                Console.WriteLine(contents);
+                userReader.Close();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
