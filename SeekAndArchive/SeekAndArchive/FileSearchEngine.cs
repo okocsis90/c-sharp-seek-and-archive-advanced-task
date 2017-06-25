eb5b23a20fbf3525fb4b8d73c98555137d4fdf8a
okocsis90 <o.kocsis90@gmail.com>
2017-06-25 18:47:01 +0200

    all feature works with seek and archive

---
+++ SeekAndArchive/SeekAndArchive/FileSearchEngine.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeekAndArchive
+{
+    class FileSearchEngine
+    {
+        private List<FileInfo> allPaths;
+        public string FileName { get; set; }
+        public string Path { get; set; }
+
+        public FileSearchEngine(string fileName, string path)
+        {
+            FileName = fileName;
+            Path = path;
+        }
+
+        public List<FileInfo> Search()
+        {
+            allPaths = new List<FileInfo>();
+            SearchEngine(Path);
+            return allPaths;
+        }
+
+        private void SearchEngine(string path)
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+
+            if (!dirInfo.Exists)
+            {
+                Console.WriteLine("The specified directory does not exist.");
+                return;
+            }
+
+            foreach (FileInfo fileInfo in dirInfo.GetFiles())
+            {
+                if (fileInfo.Name == FileName)
+                {
+                    allPaths.Add(fileInfo);
+                }
+            }
+            foreach (DirectoryInfo currentDirInfo in dirInfo.GetDirectories())
+            {
+                SearchEngine(currentDirInfo.FullName);
+            }
+        }
+    }
+}
---
+++ SeekAndArchive/SeekAndArchive/SeekAndArchive.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeekAndArchive
+{
+    class SeekAndArchive
+    {
+        static List<FileSystemWatcher> watchers = new List<FileSystemWatcher>();
+        static List<DirectoryInfo> archiveDirs;
+        static List<FileInfo> resultFiles;
+
+        static void WatcherChanged(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine("File: " + e.FullPath + " " + e.ChangeType);
+            FileSystemWatcher senderWatcher = (FileSystemWatcher)sender;
+            int index = watchers.IndexOf(senderWatcher, 0);
+            ArchiveFile(archiveDirs[index], resultFiles[index]);
+        }
+
+        static void WatcherRenamed(object source, RenamedEventArgs e)
+        {
+            Console.WriteLine("File: {0} renamed to {1}", e.OldFullPath, e.FullPath);
+        }
+
+        static void ArchiveFile(DirectoryInfo archiveDir, FileInfo fileToArchive)
+        {
+            FileStream input = File.Open(fileToArchive.FullName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            FileStream output = File.Create(archiveDir.FullName + @"\" + DateTime.Now.ToString("yyyyMMddHHmmss") + fileToArchive.Name +".gz");
+            GZipStream Compressor = new GZipStream(output, CompressionMode.Compress);
+            int b = input.ReadByte();
+            while (b != -1)
+            {
+                Compressor.WriteByte((byte)b);
+                b = input.ReadByte();
+            }
+            Compressor.Close();
+            input.Close();
+            output.Close();
+        }
+
+        static void CreateWatchers()
+        {
+            foreach (FileInfo fileInfo in resultFiles)
+            {
+                FileSystemWatcher newWatcher = new FileSystemWatcher(fileInfo.DirectoryName, fileInfo.Name);
+                newWatcher.Changed += new FileSystemEventHandler(WatcherChanged);
+                newWatcher.Renamed += new RenamedEventHandler(WatcherRenamed);
+                newWatcher.Deleted += new FileSystemEventHandler(WatcherChanged);
+                newWatcher.EnableRaisingEvents = true;
+                newWatcher.NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.Size;
+                watchers.Add(newWatcher);
+            }
+        }
+
+        static void CreateArchiveDirs()
+        {
+            archiveDirs = new List<DirectoryInfo>();
+            for (int i = 0; i < resultFiles.Count; i++)
+            {
+                archiveDirs.Add(Directory.CreateDirectory("archive" + i));
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            FileSearchEngine fileSearchEngine = new FileSearchEngine(args[0], args[1]);
+            resultFiles = fileSearchEngine.Search();
+
+            foreach (FileInfo fileInfo in resultFiles)
+            {
+                Console.WriteLine(fileInfo.FullName);
+            }
+
+            CreateWatchers();
+            CreateArchiveDirs();
+
+            Console.ReadKey();
+        }
+    }
+}
